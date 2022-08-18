037e751beb175079792c3c5ff2cc21d43eb64beb
EnzoEduardoBruno <enzoeduardobruno@gmail.com>
2022-08-18 14:07:36 -0300

    Se crea la estructura en netBeans

---
+++ src/main/java/com/enzoeduardobruno/eeb/Interface/IPersonaService.java
@@ -0,0 +1,18 @@
+package com.enzoeduardobruno.eeb.Interface;
+
+import com.enzoeduardobruno.eeb.Entity.Persona;
+import java.util.List;
+
+public interface IPersonaService {
+    //Traer una lista de personas
+    public List<Persona> GetPersona ();
+    
+    //Guardar un objeto de tipo Persona
+    public void savePersona(Persona persona);
+    
+    //Eliminar un objeto pero lo buscamos por ID
+    public void deletePersona(Long id);
+    
+    //Buscar una persona por ID
+    public Persona findPersona(Long id);
+}
