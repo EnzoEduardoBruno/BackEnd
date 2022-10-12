4f08d39653362b6ca27d6c8b1fb076daeff66c9c
EnzoEduardoBruno <enzoeduardobruno@gmail.com>
2022-10-12 16:26:14 -0300

    make it better

--- src/main/java/com/enzoeduardobruno/eeb/Dto/dtoPersona.java
+++ src/main/java/com/enzoeduardobruno/eeb/Dto/dtoPersona.java
@@ -1,53 +1,36 @@
-package com.enzoeduardobruno.eeb.Entity;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.enzoeduardobruno.eeb.Dto;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.NotBlank;
 
-@Entity
-public class Persona {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    
-    @NotNull
-    @Size(min = 1, max = 50, message = "no cumple con la longitud")
+/**
+ *
+ * @author enzoe
+ */
+public class dtoPersona {
+    @NotBlank
     private String nombre;
-    
-    @NotNull
-    @Size(min = 1, max = 50, message = "no cumple con la longitud")
+    @NotBlank
     private String apellido;
-    
-    @NotNull
-    @Size(min = 1, max = 2600, message = "no cumple con la longitud")
+    @NotBlank
     private String descripcion;
-    
-    @NotNull
-    @Size(min = 1, max = 50, message = "no cumple con la longitud")
+    @NotBlank
     private String titulo;
-    
+    @NotBlank
     private String img;
 
-    public Persona() {
+    public dtoPersona() {
     }
 
-    public Persona(String nombre, String apellido, String descripcion, String titulo, String img) {
+    public dtoPersona(String nombre, String apellido, String descripcion, String titulo, String img) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.descripcion = descripcion;
         this.titulo = titulo;
         this.img = img;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getNombre() {
@@ -89,7 +72,7 @@ public class Persona {
     public void setImg(String img) {
         this.img = img;
     }
-
-       
+    
+    
     
 }
